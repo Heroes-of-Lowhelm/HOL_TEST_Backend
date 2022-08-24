1e2f74d42a95ef279d43f3bb6de813408873f22c
asatriangrish <68015194+asatriangrish@users.noreply.github.com>
2022-08-24 11:13:22 +0400

    feat: user authentication complete

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -1,13 +1,12 @@
 const bcrypt = require("bcryptjs");
 const User = require("../models/user.model");
+const Code = require("../models/code.model")
 const {sendVerificationEmail} = require("../services/verificationService");
 
 
 exports.register = async (req, res) => {
     try {
         let { username, password, email } = req.body;
-        console.log(req.body);
-        console.log(username, password, email);
         //password encryption
         password = bcrypt.hashSync(password, 10);
 
@@ -30,9 +29,20 @@ exports.register = async (req, res) => {
                 });
         })
 
+        const code = new Code({
+            mail : email,
+            verification_code: verificationCode
+        })
+        Code.create(code, (err, data) => {
+            if (err) {
+                res.status(500).send({
+                    message: err.message || "Some error occurred while generating verification code"
+                })
+            }
+        })
         // Send verification code to the user's email
         await sendVerificationEmail(email, verificationCode);
-        
+
         // Send user id and base32 key to user
         res.status(200).send({
             message: `You have Registered Successfully, Verification code sent to: ${email}`
@@ -42,5 +52,110 @@ exports.register = async (req, res) => {
         res.status(500).json({
             message: 'Error generating secret key'
         });
+    }
+}
+
+exports.verifyAccount = async (req, res) => {
+    try {
+        let {email, code} = req.body;
+        Code.findByMail(email, (err, data) => {
+            if (err) {
+                if (err.kind === "not_found") {
+                    res.status(404).send({
+                        message: `Verification code is not generated for ${email}.`
+                    });
+                } else {
+                    res.status(500).send({
+                        message: "Error while getting activation code"
+                    });
+                }
+            } else {
+                if (data.verification_code == code) {
+                    User.activateByMail(email, (err, data) => {
+                        if (err) {
+                            res.status(500).send({
+                                message: "Can't verify the account"
+                            });
+                        } else {
+                            res.status(200).send({
+                                message: `You have verified account`
+                            })
+                        }
+                    })
+                } else {
+                    res.status(500).send({
+                        message: "Verification code is not correct"
+                    })
+                }
+            }
+        })
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            message: 'Error verifying account'
+        })
+    }
+}
+
+exports.resendCode = async (req, res) => {
+    try {
+        let { email } = req.body;
+        //verification code generation
+        const verificationCode = Math.floor(100000 + Math.random() * 900000);
+        const code = new Code({
+            mail : email,
+            verification_code: verificationCode
+        })
+        Code.create(code, (err, data) => {
+            if (err) {
+                res.status(500).send({
+                    message: err.message || "Some error occurred while generating verification code"
+                })
+            }
+        })
+        // Send verification code to the user's email
+        await sendVerificationEmail(email, verificationCode);
+
+        // Send user id and base32 key to user
+        res.status(200).send({
+            message: `Verification code sent to: ${email}`
+        })
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            message: 'Error sending verification code'
+        })
+    }
+}
+
+exports.login = (req, res) => {
+    try {
+        let {email, password} = req.body;
+        User.findByMail(email, (err, data) => {
+            if (err) {
+                if (err.kind === "not_found") {
+                    res.status(404).send({
+                        message: `There is no account which registered by ${email}.`
+                    });
+                } else {
+                    res.status(500).send({
+                        message: err.message || "Some error occurred while creating new user."
+                    });
+                }
+            } else {
+                console.log(data.password)
+                if (!bcrypt.compareSync(password, data.password)) {
+                    res.status(401).send('Invalid login credentials');
+                }
+                else {
+                    res.status(200).send(data)
+                }
+            }
+        })
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            message: 'Error while login'
+        })
     }
 }
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,32 +1,39 @@
+const {request} = require("express");
 module.exports = app => {
-  const tutorials = require("../controllers/tutorial.controller.js");
+  // const tutorials = require("../controllers/tutorial.controller.js");
   const auth = require("../controllers/auth.controller")
 
   var router = require("express").Router();
 
-  // Create a new Tutorial
-  router.post("/", tutorials.create);
-
-  // Retrieve all Tutorials
-  router.get("/", tutorials.findAll);
-
-  // Retrieve all published Tutorials
-  router.get("/published", tutorials.findAllPublished);
-
-  // Retrieve a single Tutorial with id
-  router.get("/:id", tutorials.findOne);
-
-  // Update a Tutorial with id
-  router.put("/:id", tutorials.update);
-
-  // Delete a Tutorial with id
-  router.delete("/:id", tutorials.delete);
-
-  // Delete all Tutorials
-  router.delete("/", tutorials.deleteAll);
+  // // Create a new Tutorial
+  // router.post("/", tutorials.create);
+  //
+  // // Retrieve all Tutorials
+  // router.get("/", tutorials.findAll);
+  //
+  // // Retrieve all published Tutorials
+  // router.get("/published", tutorials.findAllPublished);
+  //
+  // // Retrieve a single Tutorial with id
+  // router.get("/:id", tutorials.findOne);
+  //
+  // // Update a Tutorial with id
+  // router.put("/:id", tutorials.update);
+  //
+  // // Delete a Tutorial with id
+  // router.delete("/:id", tutorials.delete);
+  //
+  // // Delete all Tutorials
+  // router.delete("/", tutorials.deleteAll);
 
   // Register user
   router.post("/register", auth.register)
+  // Verify user
+  router.post("/verify", auth.verifyAccount)
+  // Resend code
+  router.post("/resend", auth.resendCode)
+  // Login user
+  router.post("/login", auth.login)
 
   app.use('/api', router);
 };
